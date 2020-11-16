8aadf18109d68b1dab8db1e160b29ff835699045
Matheus Mello <matheusmello_22@hotmail.com>
2020-11-15 21:28:28 -0300

    Create index/show on chat bot messages

--- app/controllers/api/v1/chat_bot_messages_controller.rb
+++ app/controllers/api/v1/chat_bot_messages_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::ChatBotMessagesController < ApplicationController
   def index
+    chat_bot_messages = ChatBotMessage.all
+    render json: chat_bot_messages
   end
 
   def show
+    chat_bot_message = ChatBotMessage.find(params[:id])
+    render json: chat_bot_message
   end
 
   def create
